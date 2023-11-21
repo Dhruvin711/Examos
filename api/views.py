a37793fdd7d00963d4cbcb15c4fdf509548e8fae
Dhruvin711 <dhruvin7117@gmail.com>
2023-11-22 00:28:49 +0530

    Updated method for edge cases of question paper generator

--- api/views.py
+++ api/views.py
@@ -63,9 +63,9 @@ def generateQuestionPaper(request):
     hard_percentage = data['hard']
 
     # Calculate the marks of each difficulty level
-    easy_marks = int(total_marks_req * (easy_percentage / 100))
-    medium_marks = int(total_marks_req * (medium_percentage / 100))
-    hard_marks = int(total_marks_req * (hard_percentage / 100))
+    easy_questions_marks = int(total_marks_req * (easy_percentage / 100))
+    medium_questions_marks = int(total_marks_req * (medium_percentage / 100))
+    hard_questions_marks = int(total_marks_req * (hard_percentage / 100))
 
     # Query questions from the database based on the difficulty level
     easy_questions = list(Question.objects.filter(difficulty='easy'))
@@ -76,38 +76,92 @@ def generateQuestionPaper(request):
 
     # Collecting easy questions for the question paper
     for question in easy_questions:
-        if question.marks <= easy_marks:
-            easy_marks -= question.marks
+        if question.marks <= easy_questions_marks:
+            easy_questions_marks -= question.marks
             question_paper.append(question)
+
+    curr_easy_marks = sum(q.marks for q in question_paper)
     
     # Collecting easy questions for the question paper
     for question in medium_questions:
-        if question.marks <= medium_marks:
-            medium_marks -= question.marks
+        if question.marks <= medium_questions_marks:
+            medium_questions_marks -= question.marks
             question_paper.append(question)
+
+    curr_medium_marks = sum(q.marks for q in question_paper) - curr_easy_marks
     
     # Collecting easy questions for the question paper
     for question in hard_questions:
-        if question.marks <= hard_marks:
-            hard_marks -= question.marks
+        if question.marks <= hard_questions_marks:
+            hard_questions_marks -= question.marks
             question_paper.append(question)
+
+    curr_hard_marks = sum(q.marks for q in question_paper) - curr_medium_marks - curr_easy_marks
 
     # Calculate the total marks of the generated question paper
     question_paper_marks = sum(question.marks for question in question_paper)
 
     # if question paper marks is less than required marks, then adding new extra questions to the question paper
-    if question_paper_marks < total_marks_req:
-        for question in (easy_questions + medium_questions + hard_questions):
-            if question not in question_paper and question.marks <= total_marks_req - question_paper_marks:
-                question_paper.append(question)
-                question_paper_marks += question.marks
+    new_easy_questions = [q for q in easy_questions if q not in question_paper]
+    new_medium_questions = [q for q in medium_questions if q not in question_paper]
+    new_hard_questions = [q for q in hard_questions if q not in question_paper]
 
+    new_easy = 0
+    new_medium = 0
+    new_hard = 0
+
+    easy_marks = easy_questions[0].marks
+    medium_marks = medium_questions[0].marks
+    hard_marks = hard_questions[0].marks
+
+    for x in range(len(new_easy_questions) + 1):
+        for y in range(len(new_medium_questions) + 1):
+            for z in range(len(new_hard_questions) + 1):
+                if easy_marks*x + medium_marks*y + hard_marks*z == total_marks_req - question_paper_marks:
+                    new_easy = x
+                    new_medium = y
+                    new_hard = z
+                    break
     
+    for question in new_easy_questions:
+        if new_easy == 0:
+            break
+
+        question_paper.append(question)
+        curr_easy_marks += question.marks
+        new_easy-=1
+
+    for question in new_medium_questions:
+        if new_medium == 0:
+            break
+    
+        question_paper.append(question)
+        curr_medium_marks += question.marks
+        new_medium-=1
+
+    for question in new_hard_questions:
+        if new_hard == 0:
+            break
+    
+        question_paper.append(question)
+        curr_hard_marks += question.marks
+        new_hard-=1
+
+    question_paper_marks = sum(question.marks for question in question_paper)
+
+    final_easy_percentage = curr_easy_marks / question_paper_marks * 100
+    final_medium_percentage = curr_medium_marks / question_paper_marks * 100
+    final_hard_percentage = curr_hard_marks / question_paper_marks * 100
+
+    # Serializing the question paper data
     serializer = QuestionSerializer(question_paper, many=True)
 
     return Response({
                         "question_paper": serializer.data,
-                        "total_marks": question_paper_marks
+                        "total_marks": question_paper_marks,
+                        "Easy Question Percentages" : final_easy_percentage,
+                        "Medium Question Percentages": final_medium_percentage,
+                        "Hard Question Percentages": final_hard_percentage,
                     }, status=status.HTTP_200_OK)
 
 def homePage(request):
